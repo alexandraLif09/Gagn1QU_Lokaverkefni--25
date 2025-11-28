c98b23f73f6923d16c247f676a52f8cbe8704c1b
Alexandra <alexadolph1701@gmail.com>
2025-11-28 11:45:33 +0000

    bjó til töflu fyrir Quidditch leiki og setti inní hana

--- create/create.sql
+++ create/create.sql
@@ -11,3 +11,11 @@ CREATE TABLE Courses (
     name VARCHAR(255),
     description TEXT
 );
+
+CREATE TABLE QGames (
+    id SERIAL PRIMARY KEY,
+    score INTEGER NOT NULL,
+    winner VARCHAR(10),
+    beginDate DATE,
+    endDate DATE
+);
