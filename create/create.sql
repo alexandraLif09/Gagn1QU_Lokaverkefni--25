ec8478cb8bf36510a9c188d894e14762e3c58ec5
Alexandra <alexadolph1701@gmail.com>
2025-12-05 10:14:08 +0000

    Bjó til töflu fyrir nemendur og setti inní hana

--- create/create.sql
+++ create/create.sql
@@ -37,3 +37,13 @@ CREATE TABLE Cores (
     name VARCHAR(255),
     description TEXT
 );
+
+CREATE TABLE Students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    year INTEGER,
+    CONSTRAINT fk_House
+        FOREIGN KEY(Houses_id)
+        REFERENCES Houses(id)
+        ON DELETE CASCADE
+);
