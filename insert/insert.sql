11039b5880511be31b144a992fbf0ac50fea47c1
Alexandra <alexadolph1701@gmail.com>
2025-12-07 14:59:24 +0000

    Gerði fyrsta insert-ið mitt í húsa töfluna

---
+++ insert/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO Houses (name, description)
+VALUES ('Gryffindor', 'A house known for their bravery, courage, determination, nerve, chivalry, boldness and loyalty. 
+        The house was founded by Godric Gryffindor and their house colors are scarlet and gold.')
+        ('Slytherin', 'A house known for their ambition, cunningness, resoursefulness and self-preservation. 
+        The house was founded by Salazar Slytherin and their house colors are emerald green and silver.')
+        ('Ravenclaw', 'A house known for their wit, wisdom and creativity. 
+        The house was founded by Rowena Ravenclaw and their house colors are blue and bronze.')
+        ('Hufflepuff', 'A house known for their hard work, dedication, loyalty and fair play although they welcome all students. 
+        The house was founded by Godric Gryffindor and their house colors are scarlet and gold.')
