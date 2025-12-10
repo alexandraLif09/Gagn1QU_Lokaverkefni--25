7e4e41872ef1c41e2684726683844735c5d853e5
Alexandra <alexadolph1701@gmail.com>
2025-12-10 09:17:58 +0000

    Gerði insert í áfanga töfluna

--- insert/insert.sql
+++ insert/insert.sql
@@ -7,3 +7,32 @@ VALUES ('Gryffindor', 'A house known for their bravery, courage, determination, 
         The house was founded by Rowena Ravenclaw and their house colors are blue and bronze.')
         ('Hufflepuff', 'A house known for their hard work, dedication, loyalty and fair play although they welcome all students. 
         The house was founded by Godric Gryffindor and their house colors are scarlet and gold.')
+
+INSERT INTO courses (name, description)
+VALUES ('Transfiguration', 'A subject where students learn to change objects form, appearance or substance 
+        into another form, appearance, or substance.'),
+        ('Charms', 'A subject where students learn spells that add properties or change what objects do.'),
+        ('Potions', 'A subject where students learn to create liquid mixtures with specific magical effects, 
+        from healing to harming.'),
+        ('Herbology', 'A subject where students study magical plants and fungi.'),
+        ('History of Magic', 'A subject where students learn significant dates, names, and events in the magical world.'),
+        ('Astronomy', 'A subject where students use telescopes to learn names, movements and properties of stars, planets
+        and moons, creating star charts, and understanding celestial phenomena.'),
+        ('Defence Against the Dark Arts (DADA)', 'A subject where students learn to protect themselves from dark magic, 
+        curses, hexes and creatures.'),
+        ('Care of Magical Creatures', 'A subject where students learn to feed, groom and handle magical beasts.'),
+        ('Divination', 'A subject where students learn to predict  the future using methods like tea-leaf reading, 
+        crystal balls, tarots, palmistry and astrology.'),
+        ('Arithmancy', 'A subject where students learn the magical significance of numbers to predict the future 
+        and understand connections between people, places and objects.'),
+        ('Muggle Studies', 'A subject where students learn about non-magical people and their lives.'),
+        ('Study of Ancient Runes', 'A subject where students learn to translate ancient runic alphabets and logograms, 
+        which are symbols representing magical concepts or words.'),
+        ('Flying class', 'A subject where students learn how to ride and control broomsticks, covering basic maneuvers, 
+        broom care and safty.'),
+        ('Apparition', 'A subject where students learn to instantly teleport but failure risks dangerous splinching
+        (leaving body parts behind).'),
+        ('Alchemy', 'A subject where students learn to transform the elements into transmute substances, 
+        like turning metal into gold.'),
+        ('Magical theory', 'A subject where students learn the why and how of magic, covering spell development, 
+        the science behind charms, magical laws, spell classification and the nature of magical energy.');
