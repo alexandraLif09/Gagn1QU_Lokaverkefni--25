629c61d2ea4175fea32e949499764be64bd32bf1
Alexandra <alexadolph1701@gmail.com>
2025-12-12 20:00:55 +0000

    Gerði select fyrirspurn sem velur alla nemendur sem eru í áfanga til að læra um töfradýr

--- select/select.sql
+++ select/select.sql
@@ -5,3 +5,10 @@ WHERE house_id = 4;
 SELECT p.name AS professor, h.name AS house
 FROM Professors p 
 JOIN Houses h ON p.house_id = h.id;
+
+SELECT s.name AS student, c.name AS course
+FROM Student_course sc
+JOIN Students s ON sc.student_id = s.id
+JOIN Courses c ON sc.course_id = c.id
+WHERE sc.course_id = '8'
+ORDER BY s.name;
