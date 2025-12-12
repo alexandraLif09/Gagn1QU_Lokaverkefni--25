998ed12ae120421142c2417f24c8f1acd29be1fb
Alexandra <alexadolph1701@gmail.com>
2025-12-12 22:20:19 +0000

    Gerði select fyrirspurn sem velur alla kennarana og segir hvað þau kenna

--- select/select.sql
+++ select/select.sql
@@ -25,3 +25,8 @@ JOIN QPlayers qp ON s.id = qp.student_id
 JOIN Student_course sc ON s.id = sc.student_id
 JOIN Courses c ON sc.course_id = c.id
 WHERE c.name = 'Potions';
+
+SELECT pr.name AS professor, co.name AS course
+FROM Professor_course pc 
+JOIN Professors pr ON pc.professor_id = pr.id
+JOIN Courses co ON pc.course_id = co.id;
